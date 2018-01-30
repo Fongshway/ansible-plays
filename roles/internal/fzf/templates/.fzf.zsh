9f7fe5f88001941b85f759d59d367e41fe327b4c
Fongshway <8451964+Fongshway@users.noreply.github.com>
2018-01-30 04:12:48 +0000

    [fzf] Add new fzf role

---
+++ roles/internal/fzf/templates/.fzf.zsh
@@ -0,0 +1,8 @@
+# {{ ansible_managed }}
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/share/fzf/key-bindings.zsh"
